6e08105b5a6b97f8bc6477a8b75c3bc7d2b1bd73
Aidan Follestad <drummer.aidan@gmail.com>
2014-11-13 13:02:36 -0600

    Fixes.

--- library/src/main/java/com/afollestad/materialdialogs/MaterialDialog.java
+++ library/src/main/java/com/afollestad/materialdialogs/MaterialDialog.java
@@ -204,7 +204,7 @@ public class MaterialDialog extends DialogBase implements View.OnClickListener, 
                 setMargin(view.findViewById(R.id.buttonDefaultFrame), -1, 0, -1, -1);
                 final int conPadding = (int) mContext.getResources().getDimension(R.dimen.md_main_frame_margin);
                 View con = view.findViewById(R.id.content);
-                con.setPadding(con.getPaddingLeft(), conPadding, con.getPaddingRight(), conPadding);
+                con.setPadding(con.getPaddingLeft(), 0, con.getPaddingRight(), conPadding);
             } else {
                 View con = view.findViewById(R.id.content);
                 con.setPadding(con.getPaddingLeft(), 0, con.getPaddingRight(), 0);
