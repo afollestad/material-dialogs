8158c1ea89f74f207ab6b81ea7745d954c94ca26
Aidan Follestad <drummer.aidan@gmail.com>
2014-12-01 22:54:19 -0600

    Release 0.3.2

---
+++ library/src/main/java/com/afollestad/materialdialogs/views/MeasureCallbackListView.java
@@ -0,0 +1,40 @@
+package com.afollestad.materialdialogs.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * @author Aidan Follestad (afollestad)
+ */
+public class MeasureCallbackListView extends ListView {
+
+    public MeasureCallbackListView(Context context) {
+        super(context);
+    }
+
+    public MeasureCallbackListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MeasureCallbackListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public static interface Callback {
+        void onMeasureScroll(ListView view);
+    }
+
+    private Callback mCallback;
+
+    public void setCallback(Callback mCallback) {
+        this.mCallback = mCallback;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (mCallback != null)
+            mCallback.onMeasureScroll(this);
+    }
+}
