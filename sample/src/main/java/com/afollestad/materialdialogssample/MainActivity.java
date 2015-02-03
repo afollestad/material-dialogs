dbee2ab4fca09a777caecf52d2a7fb8646cc9671
Aidan Follestad <drummer.aidan@gmail.com>
2015-02-02 22:47:34 -0600

    Small fix, actually release 0.6.2.2

--- sample/src/main/java/com/afollestad/materialdialogssample/MainActivity.java
+++ sample/src/main/java/com/afollestad/materialdialogssample/MainActivity.java
@@ -188,7 +188,6 @@ public class MainActivity extends ActionBarActivity implements FolderSelectorDia
                 .content(R.string.useGoogleLocationServicesPrompt)
                 .positiveText(R.string.agree)
                 .negativeText(R.string.disagree)
-                .autoDismiss(false)
                 .show();
     }
 
