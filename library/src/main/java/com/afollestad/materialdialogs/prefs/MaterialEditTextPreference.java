ec19b7ca69d1244bf924ed4a538903777c7777ce
Aidan Follestad <drummer.aidan@gmail.com>
2015-03-15 21:48:43 -0500

    Fix

--- library/src/main/java/com/afollestad/materialdialogs/prefs/MaterialEditTextPreference.java
+++ library/src/main/java/com/afollestad/materialdialogs/prefs/MaterialEditTextPreference.java
@@ -76,7 +76,7 @@ public class MaterialEditTextPreference extends DialogPreference {
 
         // Create our layout, put the EditText inside, then add to dialog
         ViewGroup layout = (ViewGroup) LayoutInflater.from(context).inflate(R.layout.md_input_dialog_stub, null);
-        mEditText.setText(mValue);
+        mEditText.append(mValue);
         layout.addView(mEditText, new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
@@ -87,7 +87,7 @@ public class MaterialEditTextPreference extends DialogPreference {
         TextView message = (TextView) layout.findViewById(android.R.id.message);
         if (getDialogMessage() != null && getDialogMessage().toString().length() > 0) {
             message.setVisibility(View.VISIBLE);
-            message.append(getDialogMessage());
+            message.setText(getDialogMessage());
         } else {
             message.setVisibility(View.GONE);
         }
