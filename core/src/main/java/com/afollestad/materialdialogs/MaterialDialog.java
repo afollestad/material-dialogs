67e0c742c7ac458aff48bebf14a06cfd58350d47
Aidan Follestad <drummer.aidan@gmail.com>
2015-09-28 13:52:18 -0500

    Reverted ButtonCallback

--- core/src/main/java/com/afollestad/materialdialogs/MaterialDialog.java
+++ core/src/main/java/com/afollestad/materialdialogs/MaterialDialog.java
@@ -1804,6 +1804,37 @@ public class MaterialDialog extends DialogBase implements
 
         public void onNeutral(MaterialDialog dialog) {
         }
+
+        // The overidden methods below prevent Android Studio from suggesting that they are overidden by developers 
+
+        public ButtonCallback() {
+            super();
+        }
+
+        @Override
+        protected final Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        @Override
+        public final boolean equals(Object o) {
+            return super.equals(o);
+        }
+
+        @Override
+        protected final void finalize() throws Throwable {
+            super.finalize();
+        }
+
+        @Override
+        public final int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public final String toString() {
+            return super.toString();
+        }
     }
 
     /**
