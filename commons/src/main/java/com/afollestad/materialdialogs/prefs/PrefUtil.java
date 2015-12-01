2996557cdf7c09e277351b8bfa6de65cd979c329
Aidan Follestad <drummer.aidan@gmail.com>
2015-12-01 10:25:43 -0600

    Resolved #851

--- commons/src/main/java/com/afollestad/materialdialogs/prefs/PrefUtil.java
+++ commons/src/main/java/com/afollestad/materialdialogs/prefs/PrefUtil.java
@@ -3,6 +3,8 @@ package com.afollestad.materialdialogs.prefs;
 import android.content.res.XmlResourceParser;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.afollestad.materialdialogs.commons.R;
@@ -17,21 +19,23 @@ class PrefUtil {
     private PrefUtil() {
     }
 
-    public static void setLayoutResource(Preference preference, AttributeSet attrs) {
+    public static void setLayoutResource(@NonNull Preference preference, @Nullable AttributeSet attrs) {
         boolean foundLayout = false;
-        for (int i = 0; i < attrs.getAttributeCount(); i++) {
-            final String namespace = ((XmlResourceParser) attrs).getAttributeNamespace(0);
-            if (namespace.equals("http://schemas.android.com/apk/res/android") &&
-                    attrs.getAttributeName(i).equals("layout")) {
-                foundLayout = true;
-                break;
+        if (attrs != null) {
+            for (int i = 0; i < attrs.getAttributeCount(); i++) {
+                final String namespace = ((XmlResourceParser) attrs).getAttributeNamespace(0);
+                if (namespace.equals("http://schemas.android.com/apk/res/android") &&
+                        attrs.getAttributeName(i).equals("layout")) {
+                    foundLayout = true;
+                    break;
+                }
             }
         }
         if (!foundLayout)
             preference.setLayoutResource(R.layout.md_preference_custom);
     }
 
-    public static void registerOnActivityDestroyListener(Preference preference, PreferenceManager.OnActivityDestroyListener listener) {
+    public static void registerOnActivityDestroyListener(@NonNull Preference preference, @NonNull PreferenceManager.OnActivityDestroyListener listener) {
         try {
             PreferenceManager pm = preference.getPreferenceManager();
             Method method = pm.getClass().getDeclaredMethod(
@@ -43,7 +47,7 @@ class PrefUtil {
         }
     }
 
-    public static void unregisterOnActivityDestroyListener(Preference preference, PreferenceManager.OnActivityDestroyListener listener) {
+    public static void unregisterOnActivityDestroyListener(@NonNull Preference preference, @NonNull PreferenceManager.OnActivityDestroyListener listener) {
         try {
             PreferenceManager pm = preference.getPreferenceManager();
             Method method = pm.getClass().getDeclaredMethod(
