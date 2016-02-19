571fa8a2d561fbe6f40d2faee9c7a6e57f904f9a
Anggrayudi H <hardiannicko@gmail.com>
2016-02-19 14:19:29 +0700

    Added TextInputLayout

--- core/src/main/java/com/afollestad/materialdialogs/MaterialDialog.java
+++ core/src/main/java/com/afollestad/materialdialogs/MaterialDialog.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.annotation.UiThread;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.res.ResourcesCompat;
 import android.text.Editable;
 import android.text.TextUtils;
