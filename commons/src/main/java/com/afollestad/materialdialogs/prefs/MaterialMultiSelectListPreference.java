d0a3a36dc56fd0d1ebb5889a702cd87d504c59f7
vFISHv <513450035@qq.com>
2018-07-16 15:09:27 +0800

    android.app.Dialog >> android.support.v7.app.AppCompatDialog

--- commons/src/main/java/com/afollestad/materialdialogs/prefs/MaterialMultiSelectListPreference.java
+++ commons/src/main/java/com/afollestad/materialdialogs/prefs/MaterialMultiSelectListPreference.java
@@ -1,7 +1,7 @@
 package com.afollestad.materialdialogs.prefs;
 
 import android.annotation.TargetApi;
-import android.app.Dialog;
+import android.support.v7.app.AppCompatDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Build;
@@ -69,7 +69,7 @@ public class MaterialMultiSelectListPreference extends MultiSelectListPreference
   }
 
   @Override
-  public Dialog getDialog() {
+  public AppCompatDialog getDialog() {
     return mDialog;
   }
 
@@ -163,7 +163,7 @@ public class MaterialMultiSelectListPreference extends MultiSelectListPreference
   @Override
   protected Parcelable onSaveInstanceState() {
     final Parcelable superState = super.onSaveInstanceState();
-    Dialog dialog = getDialog();
+    AppCompatDialog dialog = getDialog();
     if (dialog == null || !dialog.isShowing()) {
       return superState;
     }
