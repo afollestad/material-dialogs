b9161a1b2a16e9819c70f82268fc000bedfa1035
Aidan Follestad <afollestad@squareup.com>
2018-09-09 14:49:37 -0700

    Version 2.0.0-alpha08

--- core/src/main/java/com/afollestad/materialdialogs/utils/IntArrayExt.kt
+++ core/src/main/java/com/afollestad/materialdialogs/utils/IntArrayExt.kt
@@ -1,3 +1,8 @@
+/*
+ * Licensed under Apache-2.0
+ *
+ * Designed and developed by Aidan Follestad (@afollestad)
+ */
 package com.afollestad.materialdialogs.utils
 
 internal fun IntArray.appendAll(values: Collection<Int>): IntArray {
