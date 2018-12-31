8b7f4dddbb667ca8f947fdcdad4e1a64e752c685
stasbar <stasbar1995@gmail.com>
2018-12-31 01:56:18 +0100

    apply spotless

--- core/src/main/java/com/afollestad/materialdialogs/internal/fragment/MaterialDialogFragment.kt
+++ core/src/main/java/com/afollestad/materialdialogs/internal/fragment/MaterialDialogFragment.kt
@@ -1,3 +1,18 @@
+/**
+ * Designed and developed by Aidan Follestad (@afollestad)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.afollestad.materialdialogs.internal.fragment
 
 import android.app.Dialog
@@ -17,8 +32,11 @@ abstract class MaterialDialogFragment : DialogFragment() {
    * for further customizations.
    * @return Return a view that will be applied as a customView to the MaterialDialog instance.
    */
-  abstract fun onCreateView(inflater: LayoutInflater, savedInstanceState: Bundle?,
-                            dialog: MaterialDialog): View
+  abstract fun onCreateView(
+    inflater: LayoutInflater,
+    savedInstanceState: Bundle?,
+    dialog: MaterialDialog
+  ): View
 
   /**
    * This method won't be called, thus overriding in sub-classes will be source of bugs
@@ -30,8 +48,11 @@ abstract class MaterialDialogFragment : DialogFragment() {
   /**
    * Result of this method won't be used, this overriding in sub-classes will be source of bugs
    */
-  final override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                                  savedInstanceState: Bundle?): View? {
+  final override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
     return super.onCreateView(inflater, container, savedInstanceState)
   }
 
