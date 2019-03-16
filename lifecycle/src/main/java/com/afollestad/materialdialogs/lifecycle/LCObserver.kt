50802bde064b32174d6dcbe884c0f2dbb61f810b
Jordy Alexis Mendoza Caballero <jordyamc@users.noreply.github.com>
2019-03-15 21:55:28 -0500

    Implement lifecycle so before the owner is destroyed the dialog dismiss itself (#1746)
    By @jordyamc

---
+++ lifecycle/src/main/java/com/afollestad/materialdialogs/lifecycle/LCObserver.kt
@@ -0,0 +1,28 @@
+/**
+ * Designed and developed by Aidan Follestad (@afollestad)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.afollestad.materialdialogs.lifecycle
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class LCObserver(private val callback: () -> Unit) : LifecycleObserver {
+    /** dismiss automatically when lifecycle owner is destroyed. */
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        callback.invoke()
+    }
+}
