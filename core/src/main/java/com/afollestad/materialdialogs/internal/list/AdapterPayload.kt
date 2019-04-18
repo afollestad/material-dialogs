0972ce0e53290385dcacf14e8a48785d91806c28
Aidan Follestad <afollestad@squareup.com>
2019-04-18 10:44:32 -0700

    KDoc and cleanup

--- core/src/main/java/com/afollestad/materialdialogs/internal/list/AdapterPayload.kt
+++ core/src/main/java/com/afollestad/materialdialogs/internal/list/AdapterPayload.kt
@@ -15,5 +15,14 @@
  */
 package com.afollestad.materialdialogs.internal.list
 
-object CheckPayload
-object UncheckPayload
+/**
+ * Used by the single and multi-choice list adapters to selectively bind list items. This
+ * payload indicates that the notified list item should check its toggleable view.
+ */
+internal object CheckPayload
+
+/**
+ * Used by the single and multi-choice list adapters to selectively bind list items. This
+ * payload indicates that the notified list item should uncheck its toggleable view.
+ */
+internal object UncheckPayload
